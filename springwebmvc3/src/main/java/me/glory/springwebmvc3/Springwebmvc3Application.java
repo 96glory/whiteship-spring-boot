dd1009e0b597e1f111e91a09ae96f8f7b5c50778
96glory <52440668+YoungKwang-Song@users.noreply.github.com>
2020-07-16 15:35:33 +0900

    CH4: 스프링 웹 MVC: ExceptionHandler

---
+++ springwebmvc3/src/main/java/me/glory/springwebmvc3/Springwebmvc3Application.java
@@ -0,0 +1,13 @@
+package me.glory.springwebmvc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebmvc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springwebmvc3Application.class, args);
+    }
+
+}
---
+++ springwebmvc3/src/test/java/me/glory/springwebmvc3/Springwebmvc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.glory.springwebmvc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebmvc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
